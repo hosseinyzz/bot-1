7ee830203d9798c726352d7429410ff5faa39c50
hosseinyzz <hosseinyazdani1380@gmail.com>
2016-02-27 02:03:24 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {122997453,123627901,0,tonumber(our_id)},--Sudo users
+   sudo_users = {122997453,148570126,123627901,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
